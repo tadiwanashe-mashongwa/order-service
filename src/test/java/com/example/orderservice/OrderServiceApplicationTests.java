dc050cbc7a26e5bf5c8d42ef43f7be884388afff
TadiwanasheMashongwa <sbacohort@gmail.com>
2026-08-15 06:43:09 +0200

    test: use postgres container for context smoke test

--- src/test/java/com/example/orderservice/OrderServiceApplicationTests.java
+++ src/test/java/com/example/orderservice/OrderServiceApplicationTests.java
@@ -1,10 +1,11 @@
 package com.example.orderservice;
 
+import com.example.orderservice.config.AbstractPostgresContainerTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class OrderServiceApplicationTests {
+class OrderServiceApplicationTests extends AbstractPostgresContainerTest {
 
 	@Test
 	void contextLoads() {
